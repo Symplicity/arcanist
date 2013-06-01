d97ba0f9a00ae2d55803810626215a579ff090b4
Svemir Brkic <svemir.brkic@gmail.com>
2013-05-31 21:51:41 -0400

    if phpcs options are set, use the Code Sniffer

--- src/lint/engine/symp/SympArcanistLintEngine.php
+++ src/lint/engine/symp/SympArcanistLintEngine.php
@@ -5,7 +5,13 @@ class SympArcanistLintEngine extends ArcanistLintEngine {
   public function buildLinters() {
     $linters = array();
     $paths = $this->getPaths();
-    $xhpast_linter = new SympXHPASTLinter();
+    $project = $this->getWorkingCopy();
+    $config = $project->getProjectConfig();
+    if (isset($config['lint.phpcs.standard'])) {
+      $xhpast_linter = new ArcanistPhpcsLinter();
+	} else {
+      $xhpast_linter = new SympXHPASTLinter();
+	}
     $linters[] = $xhpast_linter;
     foreach ($paths as $path) {
       if (preg_match('/\.(php|class|inc)$/', $path)) {
