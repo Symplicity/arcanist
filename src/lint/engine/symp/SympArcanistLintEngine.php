fa7c6e6e4147d92c403fcbfd490ebbcad373793b
Svemir Brkic <svemir.brkic@gmail.com>
2013-06-01 23:26:48 -0400

    added optional jshint linter (requires lint.jshint.config in .arcconfig)

--- src/lint/engine/symp/SympArcanistLintEngine.php
+++ src/lint/engine/symp/SympArcanistLintEngine.php
@@ -3,23 +3,31 @@
 class SympArcanistLintEngine extends ArcanistLintEngine {
 
   public function buildLinters() {
-    $linters = array();
     $paths = $this->getPaths();
     $project = $this->getWorkingCopy();
     $config = $project->getProjectConfig();
     if (isset($config['lint.phpcs.standard'])) {
-      $xhpast_linter = new ArcanistPhpcsLinter();
+      $php_linter = new ArcanistPhpcsLinter();
     } else {
-      $xhpast_linter = new SympXHPASTLinter();
+      $php_linter = new SympXHPASTLinter();
     }
-    $linters[] = $xhpast_linter;
+    $linters = array(
+      $php_linter
+    );
+    if (isset($config['lint.jshint.config'])) {
+      $js_linter = new ArcanistJSHintLinter();
+      $linters[] = $js_linter;
+    }
+
     foreach ($paths as $path) {
+      $linter = null;
       if (preg_match('/\.(php|class|inc)$/', $path)) {
-        $data = $this->loadData($path);
-        if ($data) {
-          $xhpast_linter->addPath($path);
-          $xhpast_linter->addData($path, $data);
-        }
+        $linter = $php_linter;
+      } elseif (isset($js_linter) && preg_match('/\.js$/', $path)) {
+        $linter = $js_linter;
+      }
+      if ($linter) {
+        $linter->addPath($path);
       }
     }
     return $linters;
